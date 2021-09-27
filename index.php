a3639aad941755121808c5b035c45a3fc9e7c065
Jugesh Raghav <jugesh1520raghav@gmail.com>
2021-09-27 16:41:51 +0530

    done

--- index.php
+++ index.php
@@ -5,57 +5,81 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<!-------------------------------------------Bootstrap link----------------------------------------------------------------------------------->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+ <!-------------------------------------------Bootstrap link----------------------------------------------------------------------------------->
+ <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+
+  <!------------------------------------------Bootstrap scripts-------------------------------------------------------------------------------->
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+    integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
+    crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"
+    integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN"
+    crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js"
+    integrity="sha384-+YQ4JLhjyBLPDQt//I+STsc9iw4uQqACwlvpslubQzn4u2UU2UFM80nGisd026JF"
+    crossorigin="anonymous"></script>
+
 
 <!------------------------------------------External Stylesheet-------------------------------------------------------------------------------->
-    <link rel="stylesheet" href="public/css/style.css">
+    <link rel="stylesheet" href="public/css/main.css">
 <!------------------------------------------Font-awsome link for icons--------------------------------------------------------------------------->
     <script src="https://kit.fontawesome.com/60f0166554.js" crossorigin="anonymous"></script>
     <title>E-Library</title>
 </head>
 
 <body>
-    <div class="container">
-<!---------------------------------------------Navbar------------------------------------------------------------------------------------------------>
-        <nav class="navbar fixed-top navbar-light bg-light">
-            <div class="container-fluid">
-                <h1 class="logo">E-Library</h1>
-        </nav>
-<!----------------------------------------------Red and Black Circles for decorative purpose---------------------------------------------------------------------------->
-        <div class="red-circle circle"> </div>
-        <div class="black-circle circle"> </div>
-
-<!---------------------------------------------Sign in box -------------------------------------------------------------------------------------------->      
-        <div class="page-container">
-            <img src="Images/starting-page-image.webp" alt="starting image" width="900" height="500">
-            <div class="index-main-block">
-                <h1>Embrace The Books!</h1>
-                <div class="quote-block">
-                    <p><em>Knowledge is free at library, Just bring your own container.</em></p>
+    <!--------------------------------------------------Navbar------------------------------------------------------------------------>
+    <div>
+        <nav class="navbar navbar-expand-lg fixed-top navbar-light ">
+            <div class="container">
+                <a class="navbar-brand " href="#"><span class="nav-logo"> LearnZILLA</span> </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                    aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#home">HOME</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#projects">ABOUT US</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#pricing">SERVICES</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#team">TEAM</a>
+                        </li>
+                    </ul>
                 </div>
-                <form action="user/user_signin.php" style="display:inline-block;">
-                <button class="btn btn-lg index-page-btn"> User Login</button>
-                </form>
-               <form action="admin/admin_signin.php " style="display:inline-block;">
-               <button class="btn btn-lg index-page-btn"> Admin Login</button>
-               </form>
-                
             </div>
-        </div>
+        </nav>
     </div>
-
-<!-------------------------------------------------About Us------------------------------------------------------------------------------>
-
-<!--------------------------------------------------Contact Us------------------------------------------------------------------------------------->
-
-<!-----------------------------------------------Footer---------------------------------------------------------------------------------------------------->
-    
-    <footer class="footer">
-        <span>Copyrights @Team</span>
-    </footer>
-    <script src="index.js"></script>
+    <!------------------------------------------------HOme----------------------------------------------------------------------------->
+    <div class="home">
+    <img src="Images/homePageImage.svg" alt="main" width="700" height="900">
+     <div class="homePageForm">
+         <div class="homepage_quote">
+        
+         </div>
+         <br>
+         <div class="button_div">
+         <button class="box">
+             <a>
+                 Get access to offline Library
+             </a>
+         </button>
+         <button class="box">
+             <a>
+                 Read articles
+            </a>
+        </button>
+       </div>
+     </div>
+</div>
 </body>
 
 </html>
